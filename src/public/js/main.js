28e0812c99ba450efaaeec07d062bb328b4e403a
Christopher Thompson H <c.thompsonhuerta@gmail.com>
2019-02-19 17:49:27 -0300

    Update main.js
    Se agrega funcionalidad para que elimine a un usuario del panel de usuarios de clientes.

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -16,10 +16,21 @@ $(function(){
 
     $nickForm.submit(e => {
         e.preventDefault();
-        socket.emit('new:user',$nickname.val(),data => {
+        socket.emit('new:user',$nickname.val(), data => {
+            if(data){
+             $("#NickWrap").hide();
+             $("#contentWrap").show();
 
+            }else{
+                $nickerror.html(`
+                <div class='alert alert-danger'>
+                    That username already exits.
+                </div>
+                `);
+            }
+            $nickname.val('');
         });
-    })
+    });
 
     $messageForm.submit( e =>  {
         e.preventDefault();
@@ -28,7 +39,16 @@ $(function(){
     });
 
     socket.on('new:message',function(data){
-        $chat.append(data + '<br/>');
+        $chat.append('<b>'+data.nick + '</b>: ' + data.msg+"<br/>");
+    })
+
+    socket.on('usernames', data => {
+        let html = "";
+        for (let i = 0; i < data.length; i++) {
+            html += `<p><i class='fas fa-user'></i>&nbsp;${data[i]}</p>`
+            
+        }
+        $users.html(html);
     })
 
 })
